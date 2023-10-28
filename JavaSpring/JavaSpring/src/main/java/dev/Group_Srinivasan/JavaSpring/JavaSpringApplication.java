28043e2f4e2073864cc60a781f62a500d650d5e3
Sadhanand Srinivasan <ssrin172@asu.edu>
2023-10-27 20:41:10 -0700

    Connected Spring boot to local mysql database and entered table values

--- JavaSpring/JavaSpring/src/main/java/dev/Group_Srinivasan/JavaSpring/JavaSpringApplication.java
+++ JavaSpring/JavaSpring/src/main/java/dev/Group_Srinivasan/JavaSpring/JavaSpringApplication.java
@@ -1,16 +1,28 @@
-
 package dev.Group_Srinivasan.JavaSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @SpringBootApplication
-public class JavaSpringApplication {
+public class JavaSpringApplication implements CommandLineRunner {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
-
         SpringApplication.run(JavaSpringApplication.class, args);
-
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        String sql = "INSERT INTO user_credentials (username, password) VALUES (?, ?)";
+        int result = jdbcTemplate.update(sql,"example_user7","hashed_password7");
+        if(result > 0 ){
+            System.out.println("Rows inserted");
+        }
+    }
 }
