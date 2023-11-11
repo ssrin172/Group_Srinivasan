cbba123b9bb7d505dcd92c1ff0a5c2069f675cac
Sadhanand Srinivasan <ssrin172@asu.edu>
2023-11-10 22:10:50 -0700

    task #75 for US #11, added the controller to update story points of user stories

--- scrum-service/src/main/java/com/Group_Srinivasan/scrumservice/controller/SprintBacklogController.java
+++ scrum-service/src/main/java/com/Group_Srinivasan/scrumservice/controller/SprintBacklogController.java
@@ -1,6 +1,7 @@
 package com.Group_Srinivasan.scrumservice.controller;
 
 
+import com.Group_Srinivasan.scrumservice.database.SprintBacklogRepository;
 import com.Group_Srinivasan.scrumservice.model.SprintBacklog;
 import com.Group_Srinivasan.scrumservice.service.SprintBacklogService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/SprintBacklog")
 public class SprintBacklogController {
+    private final SprintBacklogRepository sprintBacklogRepository;
+
+    public SprintBacklogController(SprintBacklogRepository sprintBacklogRepository)
+    {
+        this.sprintBacklogRepository = sprintBacklogRepository;
+    }
+
     @Autowired
     private SprintBacklogService sprintBacklogService;
     @PostMapping("/add")
@@ -27,6 +35,16 @@ public class SprintBacklogController {
     @DeleteMapping("/deleteAll")
     public void deleteAllSprintBacklog(){
         sprintBacklogService.deleteAllSprintBacklog();
+    }
+
+    @PutMapping("/{id}")
+    public String updateSprintBacklog(@PathVariable int id, @RequestBody SprintBacklog sprintBacklog) {
+        SprintBacklog currentsprintBacklog = sprintBacklogRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentsprintBacklog.setStoryPoints(sprintBacklog.getStoryPoints());
+        currentsprintBacklog.setID(sprintBacklog.getID());
+        currentsprintBacklog.setBV(sprintBacklog.getBV());
+        currentsprintBacklog = sprintBacklogRepository.save(sprintBacklog);
+        return "Story points added to user story";
     }
 
 
