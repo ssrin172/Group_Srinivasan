ef524709209ca71cbb981aae4bda9d912c9a2dcf
rmathu19 <rmathu19@asu.edu>
2023-11-15 23:40:21 -0700

    Task#79 for US#14 DevTeam UI

---
+++ scrum-ui/src/main/java/com/group_srinivasan/scrumui/DevTeamController.java
@@ -0,0 +1,27 @@
+package com.group_srinivasan.scrumui;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.*;
+
+public class DevTeamController {
+
+    
+}
