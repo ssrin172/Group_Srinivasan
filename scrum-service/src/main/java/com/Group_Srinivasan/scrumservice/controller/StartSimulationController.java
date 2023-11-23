85fc322ca8657ca29cdb4a97220933e641e7e379
Sadhanand Srinivasan <ssrin172@asu.edu>
2023-11-22 23:57:32 -0700

    task #93 for US #3 Handle Simulate(product owner)

---
+++ scrum-service/src/main/java/com/Group_Srinivasan/scrumservice/controller/StartSimulationController.java
@@ -0,0 +1,15 @@
+package com.Group_Srinivasan.scrumservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/simulate")
+public class StartSimulationController {
+    @GetMapping("/productOwner")
+    public String displayResultProductOwner(){
+        return "Result for product owner";
+    }
+
+}
